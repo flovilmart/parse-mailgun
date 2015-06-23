918c8ad965b22a021a805745973ef071bed0045c
Florent Vilmart <florentvilmart@me.com>
2015-06-23 13:06:09 -0400

    Fixes errors

--- index.js
+++ index.js
@@ -2,18 +2,27 @@ var Parse = require("parse-cloud").Parse;
 var Buffer = require('buffer').Buffer;
 var Mailgun = {
 	sendEmail: function(params, options){
-		var url = 'https:/api.mailgun.net/v2/messages'
+		var url = 'https://api.mailgun.net/v2/'+Mailgun.domainName+'/messages'
 		console.log(url);
 		var p = new Parse.Promise();
+		var resolved;
 		var successCallback = function(httpResponse){
+			if(resolved) {
+				return;
+			}
+			resolved = true;
 			if (options && options.success) {
 				options.success(httpResponse.data);
 			}
 			p.resolve(httpResponse.data);
 		}
 
-		var errorCallback = function(){
-			f (options && options.error) {
+		var errorCallback = function(httpResponse){
+			if(resolved) {
+				return;
+			}
+			resolved = true;
+			if (options && options.error) {
 				options.error(httpResponse);
 			}
 			p.reject(httpResponse);
